21f8a54fd0ecf25d4236f6a0c52257a89a6333e7
spences10 <spences10apps@gmail.com>
2017-09-11 21:22:49 +0100

    add files query

---
+++ src/pages/my-files.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default ({ data }) => {
+  console.log(data)
+  return <div>Hello world</div>
+}
+
+export const query = graphql`
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
