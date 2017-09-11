239f4da5772638aaacc66332ae78460cf7015d50
spences10 <spences10apps@gmail.com>
2017-09-11 21:17:54 +0100

    add source file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: `Pandas Eating Lots`
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
