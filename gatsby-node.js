b41da8026371b545fcf4e9bfaf824674532085c1
spences10 <spences10apps@gmail.com>
2017-09-27 19:06:09 +0100

    make pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
@@ -28,7 +29,16 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         }
       }
     `).then((result) => {
-      console.log(JSON.stringify(result, null, 4))
+      result.data.allMarkdownRemark.edges.map(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            slug: node.fields.slug
+          }
+        })
+      })
       resolve()
     })
   })
---
+++ src/templates/blog-post.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => {
+  return <div>Hello blog post</div>
+}
