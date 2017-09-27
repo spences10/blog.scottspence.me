cb7bd3b9fef04187d46bad83ee68d6f37bbce6b3
spences10 <spences10apps@gmail.com>
2017-09-27 18:54:26 +0100

    create the pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,35 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode }) => {
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    console.log(createFilePath({ node, getNode, basePath: `pages` }))
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
   }
+}
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then((result) => {
+      console.log(JSON.stringify(result, null, 4))
+      resolve()
+    })
+  })
 }
