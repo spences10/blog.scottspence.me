60acf92e039cb609eb8ca4976eb450af168b3552
spences10 <spences10apps@gmail.com>
2017-09-27 19:09:35 +0100

    add links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import g from 'glamorous'
+import Link from 'gatsby-link'
 
 import { rhythm } from '../utils/typography'
 
 export default ({ data }) => {
-  console.log(data)
   return (
     <div>
       <g.H1 display={'inline-block'} borderBottom={'1px solid'}>
@@ -12,11 +12,13 @@ export default ({ data }) => {
       </g.H1>
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <g.H3 marginBottom={rhythm(1 / 4)}>
-            {node.frontmatter.title} <g.Span color='#BBB'>— {node.frontmatter.date}</g.Span>
-          </g.H3>
-          <p>{node.excerpt}</p>
+        <div>
+          <Link to={node.fields.slug} css={{ textDecoration: `none`, color: `inherit` }}>
+            <g.H3 marginBottom={rhythm(1 / 4)}>
+              {node.frontmatter.title} <g.Span color='#BBB'>— {node.frontmatter.date}</g.Span>
+            </g.H3>
+            <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -29,10 +31,12 @@ export const query = graphql`
       totalCount
       edges {
         node {
-          id
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
