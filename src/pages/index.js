4084e7dd0fd087c4617e818f378d760fff845a07
spences10 <spences10apps@gmail.com>
2017-09-27 18:31:26 +0100

    sort order

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query IndexQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
