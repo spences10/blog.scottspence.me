1063aa97ebbbf06b65498ee8259b068f40718b4e
spences10 <spences10apps@gmail.com>
2017-09-27 18:26:34 +0100

    add things 🙃

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: `${__dirname}/src/`
       }
     },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
