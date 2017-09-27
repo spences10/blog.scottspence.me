94e8307989cb112d6e75454e7fe87b781ff27bab
spences10 <spences10apps@gmail.com>
2017-09-27 18:30:04 +0100

    add new post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import g from 'glamorous'
 
-export default () => (
-  <div>
-    <h1>Amazing Pandas Eating Things</h1>
+import { rhythm } from '../utils/typography'
+
+export default ({ data }) => {
+  console.log(data)
+  return (
     <div>
-      <img src='https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg' />
+      <g.H1 display={'inline-block'} borderBottom={'1px solid'}>
+        Amazing Pandas Eating Things
+      </g.H1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <g.H3 marginBottom={rhythm(1 / 4)}>
+            {node.frontmatter.title} <g.Span color='#BBB'>— {node.frontmatter.date}</g.Span>
+          </g.H3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
-  </div>
-)
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
