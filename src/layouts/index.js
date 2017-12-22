b5d1911129b88407a1e51e8ffd6b6806934bddc4
spences10 <spences10apps@gmail.com>
2017-12-22 18:15:09 +0000

    try this shit all again

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,39 +1,64 @@
 import React from 'react'
-import g from 'glamorous'
-import { css } from 'glamor'
+import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
 
-import { rhythm } from '../utils/typography'
+import './index.css'
 
-// Adds syntax highlighting to code blocks in markdown files using PrismJS.
-// https://www.gatsbyjs.org/packages/gatsby-remark-prismjs/
-require('prismjs/themes/prism-okaidia.css')
-
-const linkStyle = css({ float: `right`, padding: `0px 20px 0px 20px` })
-
-export default ({ children, data }) => (
-  <g.Div margin={`0 auto`} maxWidth={900} padding={rhythm(2)} paddingTop={rhythm(1.5)}>
-    <Link to={`/`}>
-      <g.H3 marginBottom={rhythm(2)} display={`inline-block`}>
-        {data.site.siteMetadata.title}
-      </g.H3>
-    </Link>
-    <Link className={linkStyle} to={`/about/`}>
-      About
-    </Link>
-    <Link className={linkStyle} to={`/contact/`}>
-      Contact
-    </Link>
-    {children()}
-  </g.Div>
+const Header = () => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          Gatsby
+        </Link>
+      </h1>
+    </div>
+  </div>
 )
 
-export const query = graphql`
-  query LayoutQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
--- src/utils/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from 'typography'
-import typogTheme from 'typography-theme-irving'
-
-const typography = new Typography(typogTheme)
-
-module.exports = typography
