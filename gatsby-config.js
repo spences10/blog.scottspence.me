86170520e097a8b1ec80c94c3d3ea7af1eb5118f
spences10 <spences10apps@gmail.com>
2017-12-22 18:21:25 +0000

    add config info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Scott Spence blog made with Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`
+      }
+    }
+  ]
 }
