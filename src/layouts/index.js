56da88a95b9c703b91cca6aa7234b24f597ef774
spences10 <spences10apps@gmail.com>
2017-12-23 09:58:24 +0000

    remove css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,39 +4,8 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import styled, { ThemeProvider } from 'styled-components'
 
-import './index.css'
-
 import { theme } from '../theme/globalStyle'
 import Header from './components/Header'
-
-// const Header = () => (
-//   <div
-//     style={{
-//       background: 'rebeccapurple',
-//       marginBottom: '1.45rem'
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: '0 auto',
-//         maxWidth: 960,
-//         padding: '1.45rem 1.0875rem'
-//       }}
-//     >
-//       <h1 style={{ margin: 0 }}>
-//         <Link
-//           to="/"
-//           style={{
-//             color: 'white',
-//             textDecoration: 'none'
-//           }}
-//         >
-//           Scott Spence
-//         </Link>
-//       </h1>
-//     </div>
-//   </div>
-// )
 
 const PageContainer = styled.div`
   display: grid;
@@ -44,14 +13,13 @@ const PageContainer = styled.div`
   grid-template-rows: auto;
   grid-template-areas:
     'hd hd hd hd hd hd hd hd hd hd hd hd'
-    'main main main main main main main main main main main main'
+    '. . main main main main main main main main . .'
     'ft ft ft ft ft ft ft ft ft ft ft ft';
 `
 
 // min-height is just for the purpose of nice looking page
 const Main = styled.div`
   grid-area: main;
-  grid-template-columns: repeat(5, 1fr);
   min-height: 800px;
   margin-top: 4.625rem;
 `
