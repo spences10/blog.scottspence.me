79965eb35ede816ae1d6c31120830360f2897355
spences10 <spences10apps@gmail.com>
2017-12-23 08:28:21 +0000

    add scott

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -27,7 +27,7 @@ const Header = () => (
             textDecoration: 'none'
           }}
         >
-          Gatsby
+          Scott Spence
         </Link>
       </h1>
     </div>
@@ -37,7 +37,7 @@ const Header = () => (
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Scott Spence blog"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' }
