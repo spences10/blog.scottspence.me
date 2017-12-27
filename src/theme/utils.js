d7a14b8c69febfd132436944aa59f0f0d2df3a9d
spences10 <spences10apps@gmail.com>
2017-12-27 08:11:41 +0000

    add utils

---
+++ src/theme/utils.js
@@ -0,0 +1,24 @@
+import { css } from 'styled-components'
+
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+  phone: 376
+}
+
+// Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `
+
+  return acc
+}, {})
+
+// export const Content = styled.div`
+//   ${media.desktop`background: dodgerblue;`};
+//   ${media.tablet`background: mediumseagreen;`};
+//   ${media.phone`background: palevioletred;`};
+// `
