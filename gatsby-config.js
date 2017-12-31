b17a9b13a9823eb903aa6ace6ba17c35c7e12bc8
spences10 <spences10apps@gmail.com>
2017-12-31 11:12:11 +0000

    add maxWidth, !comma dangle

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
+              maxWidth: 590,
               linkImagesToOriginal: false
             }
           }
