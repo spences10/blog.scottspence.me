5d010ec2f3ff9ec448377a246a4434b4a6c94490
spences10 <spences10apps@gmail.com>
2018-01-03 18:57:25 +0000

    add icons+footer

--- src/layouts/components/Footer.js
+++ src/layouts/components/Footer.js
@@ -2,69 +2,60 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
-import { StyledH1, media } from '../../theme/globalStyle'
-// import PageNav from './PageNav.js'
+import { StyledP, media } from '../../theme/globalStyle'
+import { ICONS } from '../../theme/constants'
+import Icon from './Icon'
 
 const StyledFooter = styled.div`
   z-index: 1;
   position: fixed;
   bottom: 0;
   width: 100%;
-  grid-area: f;
+  background-color: ${({ theme }) => theme.white};
+  border-top: 1px solid ${({ theme }) => theme.primary.light};
+
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: auto;
-  grid-template-areas: '. . . f f f f f f . . .';
+  grid-template-areas: '. . . . g t e l . . . .';
   ${media.giant`
-    grid-template-areas:
-      '. . f f f f f f f f . .';
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-areas:
+      '. . . g t e l . . .';
     /* background: goldenrod; */
   `};
   ${media.desktop`
-    grid-template-areas:
-      '. . f f f f f f f f. .';
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-areas:
+      '. . . g t e l . . .';
     /* background: dodgerblue; */
   `};
   ${media.tablet`
-  grid-template-columns: repeat(9, 1fr);
+  grid-template-columns: repeat(10, 1fr);
   grid-template-areas:
-      '. f f f f f f f .';
+      '. . . g t e l . . .';
     /* background: mediumseagreen; */
   `};
   ${media.phone`
-  grid-template-columns: repeat(9, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   grid-template-areas:
-      'f f f f f f f f';
+      ' . g t e l . ';
     /* background: palevioletred; */
   `};
-
-  background-color: ${({ theme }) => theme.white};
-  border-top: 1px solid ${({ theme }) => theme.primary.light};
 `
 
-const BrandTitle = StyledH1.extend`
-  grid-area: f;
+const IconWrapper = styled.div`
+  height: 100%;
   color: ${({ theme }) => theme.primary.light};
+  grid-area: ${props => props.area};
   /* top right bottom left */
   margin: 0rem 0.5rem 0rem 0.5rem;
   padding: 0rem 0.5rem 0rem 0.5rem;
-  font-family: Source Sans Pro;
+  font-size: 2rem;
   font-weight: bold;
-  ${media.giant`
-    font-size: 2rem;
-  `};
-  ${media.desktop`
-    font-size: 2rem;
-  `};
-  ${media.tablet`
-    font-size: 2rem;
-  `};
-  ${media.phone`
-    font-size: 2rem;
-  `};
 `
 
-const BrandLink = styled(Link)`
+const IconLink = styled.a`
   color: inherit;
   &:visited,
   &:active {
@@ -77,10 +68,57 @@ const BrandLink = styled(Link)`
 
 const Footer = () => (
   <StyledFooter>
-    <BrandTitle>
-      <BrandLink to="/">blog.scottspence.me</BrandLink>
-    </BrandTitle>
-    {/* <PageNav header={true} /> */}
+    <IconWrapper area={'g'}>
+      <IconLink
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/spences10"
+      >
+        <Icon icon={ICONS.GITHUB} size={20} color="#453463" />
+      </IconLink>
+    </IconWrapper>
+    <IconWrapper area={'t'}>
+      <IconLink
+        target="_blank"
+        rel="noopener"
+        href="https://twitter.com/ScottDevTweets"
+      >
+        <Icon
+          viewbox="0 0 32 32"
+          size={20}
+          color="#453463"
+          icon={ICONS.TWITTER}
+        />
+      </IconLink>
+    </IconWrapper>
+    <IconWrapper area={'e'}>
+      <IconLink
+        target="_blank"
+        rel="noopener"
+        href="mailto:spences10apps@gmail.com"
+      >
+        <Icon
+          viewbox="0 0 32 32"
+          size={20}
+          color="#453463"
+          icon={ICONS.EMAIL}
+        />
+      </IconLink>
+    </IconWrapper>
+    <IconWrapper area={'l'}>
+      <IconLink
+        target="_blank"
+        rel="noopener"
+        href="https://www.linkedin.com/in/linkedinscottspence/"
+      >
+        <Icon
+          viewbox="0 0 32 32"
+          size={20}
+          color="#453463"
+          icon={ICONS.LINKEDIN}
+        />
+      </IconLink>
+    </IconWrapper>
   </StyledFooter>
 )
 
