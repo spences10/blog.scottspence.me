1a603b8097d70d8598b5330673ba1a44db0e18b7
spences10 <spences10apps@gmail.com>
2018-01-09 14:04:30 +0000

    better slug

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,11 @@
-export const slugIt = str => {
-  return str.replace(/['’]/g, '').replace(/[^a-z0-9]+/gi, '-')
+export const slugIt = text => {
+  if (!text) return
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
 }
