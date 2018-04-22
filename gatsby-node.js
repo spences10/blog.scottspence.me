1db64f44cac8bba9826dd81152f6c46346cad5b8
spences10 <spences10apps@gmail.com>
2018-04-22 18:46:52 +0100

    log out path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,96 +1,47 @@
 const path = require('path')
+const queryAll = require('./gatsby/queryAll.js')
 
-const createTagPages = (createPage, posts) => {
-  const tagPageTemplate = path.resolve('src/templates/tags.js')
-  const allTagsTemplate = path.resolve('src/templates/all-tags.js')
-
-  const postsByTags = {}
-
-  posts.forEach(({ node }) => {
-    if (node.frontmatter.tags) {
-      node.frontmatter.tags.forEach(tag => {
-        if (!postsByTags[tag]) {
-          postsByTags[tag] = []
-        }
-
-        postsByTags[tag].push(node)
-      })
-    }
-  })
-
-  const tags = Object.keys(postsByTags)
-
-  createPage({
-    path: '/tags',
-    component: allTagsTemplate,
-    context: {
-      tags: tags.sort()
-    }
-  })
-
-  tags.forEach(tagName => {
-    const posts = postsByTags[tagName]
-
-    createPage({
-      path: `/tags/${tagName}`,
-      component: tagPageTemplate,
-      context: {
-        posts,
-        tagName
-      }
-    })
-  })
+exports.onCreateNode = ({ node }) => {
+  console.log('onCreateNode:', node.internal.type)
 }
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
-  const blogPostTemplate = path.resolve('src/templates/blog-post.js')
+  return new Promise((resolve, reject) => {
+    const pageDetailTemplate = path.resolve(
+      './src/templates/blog-post.js'
+    )
 
-  // returns promise that will start with this graphql query
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: ASC, fields: [frontmatter___date] }
-        filter: { frontmatter: { published: { eq: true } } }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            html
-            id
-            frontmatter {
-              date
-              path
-              title
-              excerpt
-              tags
-            }
-          }
+    resolve(
+      graphql(queryAll).then(result => {
+        if (result.errors) {
+          reject(result.errors)
         }
-      }
-    }
-  `).then(result => {
-    if (result.errors) return Promise.reject(result.errors)
 
-    const posts = result.data.allMarkdownRemark.edges
+        // console.log('=====================')
+        // console.log('result', result.data.allPosts.edges)
+        // console.log('=====================')
 
-    createTagPages(createPage, posts)
+        const pages = result.data.allPosts.edges
 
-    // Create pages for each markdown file.
-    posts.forEach(({ node }, index) => {
-      const prev = index === 0 ? false : posts[index - 1].node
-      const next =
-        index === posts.length - 1 ? false : posts[index + 1].node
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        context: {
-          prev,
-          next
-        }
+        // console.log('====================')
+        // console.log(pages)
+        // console.log('====================')
+
+        pages.map(({ node: page }) => {
+          const path = `/${page.slug}`
+          console.log('=====================')
+          console.log(path)
+          console.log('=====================')
+          // createPage({
+          //   path,
+          //   component: pageDetailTemplate,
+          //   context: {
+          //     slug: page.slug
+          //   }
+          // })
+        })
       })
-    })
-
-    return posts
+    )
   })
 }
