3020f6f798ad89bf7eaa9408b7e09e028af6ef93
spences10 <spences10apps@gmail.com>
2018-06-02 07:30:33 +0100

    add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
         icon: 'src/img/favicon.png' // This path is relative to the root of the site.
       }
     },
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    'gatsby-plugin-styled-components'
   ]
 }
