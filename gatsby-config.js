f079fe893e0e1deb470b56ec6ec2d5029b325e63
spences10 <spences10apps@gmail.com>
2018-06-02 06:05:58 +0100

    add image support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,29 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-transformer-remark',
+    'gatsby-plugin-react-next',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
         path: `${__dirname}/src`
+      }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590
+            }
+          }
+        ]
       }
     }
   ]
