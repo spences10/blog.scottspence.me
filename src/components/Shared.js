5b7f7444ef53c31a479d267693fe49d1d75a02a4
spences10 <spences10apps@gmail.com>
2018-06-02 11:00:56 +0100

    add shared components

---
+++ src/components/Shared.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import { media } from '../theme/globalStyle'
+
+export const PageContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas: 'h h h h h h';
+`
