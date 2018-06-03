33a51c2894d6025d541c0afb3d6b0cb8020602af
spences10 <spences10apps@gmail.com>
2018-06-03 11:02:39 +0100

    add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
 import { Wrapper } from './Shared'
 
 import { nameContent } from '../theme/constants'
 
+const BrandLink = styled(Link)`
+  color: inherit;
+  &:visited,
+  &:active {
+    color: inherit;
+  }
+  &:hover {
+    color: ${({ theme }) => theme.primaryAccent};
+  }
+`
+
 const Header = () => {
   return (
     <Wrapper area={'h'}>
-      <h1>{nameContent}</h1>
+      <BrandLink to="/">
+        <h1>{nameContent}</h1>
+      </BrandLink>
     </Wrapper>
   )
 }
