1fe9548ed6c76a2cef3daf7df8b99cab9943f237
spences10 <spences10apps@gmail.com>
2018-06-03 11:09:04 +0100

    styled unused for now

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
 import { Wrapper } from './Shared'
 
