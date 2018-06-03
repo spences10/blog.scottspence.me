ff9edbac6cdb3d44bd749a63790d985c8a48883c
spences10 <spences10apps@gmail.com>
2018-06-03 08:07:18 +0100

    add main section

--- src/components/Shared.js
+++ src/components/Shared.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
-import { media } from '../theme/globalStyle'
+// import { media } from '../theme/globalStyle'
 
 export const PageContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   grid-template-rows: auto;
-  grid-template-areas: 'h h h h h h';
+  grid-template-areas:
+    'h h h h h h'
+    'm m m m m m';
 `
