6065f6e3aaafffe9942e4cc64266654b2045298d
spences10 <spences10apps@gmail.com>
2018-06-06 06:14:49 +0100

    add styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import { withRouteData, Link } from 'react-static'
+import styled from 'styled-components'
+
+const PostsList = styled.ul`
+  margin: 1rem;
+  padding: 1rem;
+`
+
+const PostWrapper = styled.li`
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid ${({ theme }) => theme.border};
+  border-radius: 4px;
+`
+
+const PostTitle = styled.h1`
+  margin: 0.25rem 0.5rem 0.25rem 0.5rem;
+  padding: 0.5rem 0.25rem 0.5rem 0.25rem;
+  font-family: ${props => props.theme.fontHeader};
+  color: ${({ theme }) => theme.fontLight};
+  font-weight: bold;
+  /* font-size: 1rem; */
+  &:hover {
+    transform: skew(2deg); /* SKEW */
+    text-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  }
+`
 
 export default withRouteData(({ allPosts }) => (
-  <section>
-    <ul className="home-ul">
+  <React.Fragment>
+    <PostsList className="home-ul">
       {allPosts.map(post => (
-        <li className="home-li" key={post.id}>
+        <PostWrapper className="home-li" key={post.id}>
           <Link to={`/${post.slug}`} className="home-link">
-            <h3>{post.title}</h3>
+            <PostTitle>{post.title}</PostTitle>
           </Link>
-        </li>
+        </PostWrapper>
       ))}
-    </ul>
-  </section>
+    </PostsList>
+  </React.Fragment>
 ))
