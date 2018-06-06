bfd2234d24f75166c1e5c8688bda8f3af33f3ff2
spences10 <spences10apps@gmail.com>
2018-06-06 05:22:57 +0100

    add gqlrequest

--- static.config.js
+++ static.config.js
@@ -1,40 +1,65 @@
 import axios from 'axios'
 import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
+import { request } from 'graphql-request'
+
+const GRAPHCMS_API =
+  'https://api.graphcms.com/simple/v1/cjf55g4211ici0162l1bq479e'
+
+const query = `
+  {
+    allPosts {
+      id
+      slug
+      title
+      content
+    }
+    allAuthors {
+      id
+      name
+      avatar {
+        handle
+      }
+      bibliography
+    }
+  }
+`
 
 export default {
   getSiteData: () => ({
-    title: 'React Static',
+    title: 'blog.scottspence.me'
   }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const { allPosts, allAuthors } = await request(
+      GRAPHCMS_API,
+      query
+    )
     return [
       {
         path: '/',
         component: 'src/containers/Home',
+        getData: () => ({
+          allPosts
+        }),
+        children: allPosts.map(post => ({
+          path: `/${post.slug}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post
+          })
+        }))
       },
       {
         path: '/about',
         component: 'src/containers/About',
-      },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
         getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
+          allAuthors
+        })
       },
       {
         is404: true,
-        component: 'src/containers/404',
-      },
+        component: 'src/containers/404'
+      }
     ]
   },
   renderToHtml: (render, Comp, meta) => {
@@ -44,21 +69,22 @@ export default {
     return html
   },
   Document: class CustomHtml extends Component {
-    render () {
-      const {
-        Html, Head, Body, children, renderMeta,
-      } = this.props
+    render() {
+      const { Html, Head, Body, children, renderMeta } = this.props
 
       return (
         <Html>
           <Head>
             <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
             {renderMeta.styleTags}
           </Head>
           <Body>{children}</Body>
         </Html>
       )
     }
-  },
+  }
 }
