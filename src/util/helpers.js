9a447b6d7e180af8b2617bda106d40a5ab810556
spences10 <spences10apps@gmail.com>
2018-06-08 14:55:19 +0100

    add date format finction

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,9 @@
+import { format, isValid } from 'date-fns'
+
+export const formatDate = dateIn => {
+  return format(dateIn, 'YYYY MMM Do')
+}
+
 export const excerpt = (string, length = 250) => {
   if (string.length > length) {
     return `${string.substring(0, length)}...`
