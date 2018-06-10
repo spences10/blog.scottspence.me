08ef98e75c8e72c95b1a0cb1d0225affc2f0f640
spences10 <spences10apps@gmail.com>
2018-06-10 16:05:21 +0100

    add helmet post title

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
 import Markdown from 'react-markdown'
+import Helmet from 'react-helmet'
+
+import { nameContent } from '../siteMeta'
 
 export default withRouteData(({ post }) => (
   <article>
+    <Helmet title={`${post.title} - ${nameContent}`} />
     <h1>{post.title}</h1>
     <Markdown source={post.content} escapeHtml={false} />
   </article>
