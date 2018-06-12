4226c49d7357a6c0793921733d4bd72fe7eba711
spences10 <spences10apps@gmail.com>
2018-06-12 21:45:48 +0100

    start style nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,24 +6,36 @@ import styled from 'styled-components'
 import { siteUrl } from '../siteMeta'
 
 const NavWrapper = styled.nav`
+  grid-area: n;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas: '. . . b b b b b b . . .';
+  z-index: 1;
   top: 0;
   width: 100%;
   position: fixed;
+  display: grid;
   background: ${props => props.theme.primary};
-  a {
-    color: ${props => props.theme.fontDark};
-    padding: 1rem;
-    display: inline-block;
-  }
+  color: ${props => props.theme.fontDark};
+  padding: 1rem;
+`
+
+const LinksWrapper = styled.div`
+  font-size: 2rem;
+  font-weight: 700;
+  font-family: ${props => props.theme.fontHeader};
+  grid-area: b;
 `
 
 const Nav = props => {
   return (
     <NavWrapper>
-      <Link exact to="/">
-        {siteUrl.substring(8)}
-      </Link>
-      <Link to="/about">About</Link>
+      <LinksWrapper>
+        <Link exact to="/">
+          {siteUrl.substring(8)}
+        </Link>
+        <Link to="/about">About</Link>
+      </LinksWrapper>
     </NavWrapper>
   )
 }
