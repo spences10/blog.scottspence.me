8e5c104f73a259988130f9e9dab85f6ffa46b3ff
spences10 <spences10apps@gmail.com>
2018-06-17 12:00:28 +0100

    render links

--- src/containers/Tag.js
+++ src/containers/Tag.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withRouteData } from 'react-static'
+import { withRouteData, Link } from 'react-static'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
 
@@ -13,14 +13,18 @@ const PageWrapper = styled.div`
 
 export default withRouteData(({ tag }) => {
   const { blogPosts } = tag
-  console.log('=====================')
-  console.log(blogPosts)
-  console.log('=====================')
   return (
     <PageWrapper>
       {/* <Helmet title={`Posts relating to ${tag.name}`} /> */}
-
-      {console.log(typeof tag)}
+      {/* {console.log(blogPosts[0].slug)} */}
+      {blogPosts.map((post, index) => {
+        console.log(post.slug)
+        return (
+          <li key={index}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        )
+      })}
       <Dump tag={tag} />
     </PageWrapper>
   )
