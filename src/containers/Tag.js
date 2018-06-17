34b899b621d93b5a1fd21b207fe7ea4b44532eeb
spences10 <spences10apps@gmail.com>
2018-06-17 09:11:18 +0100

    add tag container

---
+++ src/containers/Tag.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { withRouteData } from 'react-static'
+import styled from 'styled-components'
+import Helmet from 'react-helmet'
+
+import { nameContent } from '../siteMeta'
+import { Dump } from '../util/helpers'
+
+const PageWrapper = styled.div`
+  grid-area: m;
+  /* height: 100vh; */
+`
+
+export default withRouteData(({ tag }) => (
+  <PageWrapper>
+    {/* <Helmet title={`Posts relating to ${tag.name}`} /> */}
+    <Dump tag={tag} />
+  </PageWrapper>
+))
