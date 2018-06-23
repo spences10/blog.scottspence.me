49612d340646450d0421b6428edeefaf3f4ef02c
spences10 <spences10apps@gmail.com>
2018-06-23 18:14:21 +0100

    staging flag

--- static.config.js
+++ static.config.js
@@ -41,7 +41,7 @@ const query = `
 
 export default {
   // siteRoot: 'https://blog.scottspence.me',
-  // stagingSiteRoot: 'https://blog-scottspence.now.sh',
+  // stagingSiteRoot: 'https://blog-scottspence.now.sh', // use --staging flag
   extractCssChunks: true,
   inlineCss: true,
   // bundleAnalyzer: true,
