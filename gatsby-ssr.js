059a14e656d0c80b69e134d32d0696405fb4984d
spences10 <spences10apps@gmail.com>
2018-07-25 17:14:01 +0100

    with ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,34 +1,34 @@
-// 'use strict'
+'use strict'
 
-// var _react = require('react')
+var _react = require('react')
 
-// var _react2 = _interopRequireDefault(_react)
+var _react2 = _interopRequireDefault(_react)
 
-// var _styledComponents = require('styled-components')
+var _styledComponents = require('styled-components')
 
-// var _server = require('react-dom/server')
+var _server = require('react-dom/server')
 
-// function _interopRequireDefault(obj) {
-//   return obj && obj.__esModule ? obj : { default: obj }
-// }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
 
-// exports.replaceRenderer = function(_ref) {
-//   var bodyComponent = _ref.bodyComponent,
-//     replaceBodyHTMLString = _ref.replaceBodyHTMLString,
-//     setHeadComponents = _ref.setHeadComponents
+exports.replaceRenderer = function(_ref) {
+  var bodyComponent = _ref.bodyComponent,
+    replaceBodyHTMLString = _ref.replaceBodyHTMLString,
+    setHeadComponents = _ref.setHeadComponents
 
-//   var sheet = new _styledComponents.ServerStyleSheet()
+  var sheet = new _styledComponents.ServerStyleSheet()
 
-//   var app = _react2.default.createElement(
-//     _styledComponents.StyleSheetManager,
-//     { sheet: sheet.instance },
-//     bodyComponent
-//   )
+  var app = _react2.default.createElement(
+    _styledComponents.StyleSheetManager,
+    { sheet: sheet.instance },
+    bodyComponent
+  )
 
-//   var body = (0, _server.renderToString)(app)
+  var body = (0, _server.renderToString)(app)
 
-//   replaceBodyHTMLString(body)
-//   setHeadComponents([sheet.getStyleElement()])
+  replaceBodyHTMLString(body)
+  setHeadComponents([sheet.getStyleElement()])
 
-//   return
-// }
+  return
+}
