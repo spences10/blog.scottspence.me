f2c34b869438794da8c78b29b6114e5b3d3641ea
spences10 <spences10apps@gmail.com>
2018-08-08 13:40:37 +0100

    add search plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,21 @@ module.exports = {
           minify: true
         }
       }
+    },
+    {
+      resolve: `@andrew-codes/gatsby-plugin-elasticlunr-search`,
+      options: {
+        // Fields to index
+        fields: ['title', 'keywords'],
+        // How to resolve each field's value for a supported node type
+        resolvers: {
+          // For any node of type MarkdownRemark, list how to resolve the fields' values
+          MarkdownRemark: {
+            title: node => node.frontmatter.title,
+            keywords: node => node.frontmatter.keywords
+          }
+        }
+      }
     }
   ]
 }
