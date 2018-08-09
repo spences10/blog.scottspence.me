cc54a27e9e2c1520653bb89406e8447ed909cef3
spences10 <spences10apps@gmail.com>
2018-08-09 09:48:32 +0100

    default config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,21 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter'
+  },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-react-next',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'src',
-        path: `${__dirname}/src`
-      }
-    },
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590
-            }
-          },
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              classPrefix: 'language-'
-            }
-          }
-        ]
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'blog.scottspence.me',
-        short_name: 'blog.scottspence.me',
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
         start_url: '/',
-        background_color: '#f7f0eb', // #755f9f
-        theme_color: '#755f9f',
+        background_color: '#663399',
+        theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/img/favicon.png' // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-offline'
   ]
 }
