7f88d472dadf9bd7be26439726edb42cb8200f5b
spences10 <spences10apps@gmail.com>
2018-08-10 08:18:55 +0100

    no css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+// import './layout.css'
 
 const Layout = ({ children, data }) => (
   <StaticQuery
