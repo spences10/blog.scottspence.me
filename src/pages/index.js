b357b853de893c270a233fa03042649a71840270
spences10 <spences10apps@gmail.com>
2018-08-10 07:01:46 +0100

    add v2 default layout and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,88 @@
 import React from 'react'
-// import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
-import { ThemeProvider } from 'styled-components'
+import { Link } from 'gatsby'
 
-import GlobalLayout from '../components/GlobalLayout'
+import Layout from '../components/layout'
 
-// import { PageContainer } from '../components/Shared'
-import {
-  BlogThemeContext,
-  BlogThemeProvider
-} from '../components/BlogThemeContext'
-
-import { siteMeta, nameContent } from '../theme/constants'
-
-import Posts from '../components/Posts'
-/**
- * other themes
- * dark
- * funky
- * okaidia
- * coy
- * solarizedlight
- * tomorrow
- * twilight
- * prism.css = default
- */
-require('prismjs/themes/prism-solarizedlight.css')
-
-const IndexPage = ({ data }) => {
-  const { edges: posts } = data.allMarkdownRemark
-  return (
-    <BlogThemeProvider>
-      <BlogThemeContext.Consumer>
-        {({ theme }) => (
-          <ThemeProvider theme={theme}>
-            <GlobalLayout>
-              <Helmet title={nameContent} meta={siteMeta} />
-              <Posts posts={posts} />
-            </GlobalLayout>
-          </ThemeProvider>
-        )}
-      </BlogThemeContext.Consumer>
-    </BlogThemeProvider>
-  )
-}
-
-export const query = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { frontmatter: { published: { eq: true } } }
-    ) {
-      totalCount
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            title
-            date(formatString: "YYYY MMMM Do")
-            path
-            tags
-            excerpt
-          }
-        }
-      }
-    }
-  }
-`
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
 
 export default IndexPage
+
+// import React from 'react'
+// // import Link from 'gatsby-link'
+// import Helmet from 'react-helmet'
+// import { ThemeProvider } from 'styled-components'
+
+// import GlobalLayout from '../components/GlobalLayout'
+
+// // import { PageContainer } from '../components/Shared'
+// import {
+//   BlogThemeContext,
+//   BlogThemeProvider
+// } from '../components/BlogThemeContext'
+
+// import { siteMeta, nameContent } from '../theme/constants'
+
+// import Posts from '../components/Posts'
+// /**
+//  * other themes
+//  * dark
+//  * funky
+//  * okaidia
+//  * coy
+//  * solarizedlight
+//  * tomorrow
+//  * twilight
+//  * prism.css = default
+//  */
+// require('prismjs/themes/prism-solarizedlight.css')
+
+// const IndexPage = ({ data }) => {
+//   const { edges: posts } = data.allMarkdownRemark
+//   return (
+//     <BlogThemeProvider>
+//       <BlogThemeContext.Consumer>
+//         {({ theme }) => (
+//           <ThemeProvider theme={theme}>
+//             <GlobalLayout>
+//               <Helmet title={nameContent} meta={siteMeta} />
+//               <Posts posts={posts} />
+//             </GlobalLayout>
+//           </ThemeProvider>
+//         )}
+//       </BlogThemeContext.Consumer>
+//     </BlogThemeProvider>
+//   )
+// }
+
+// export const query = graphql`
+//   query IndexQuery {
+//     allMarkdownRemark(
+//       sort: { order: DESC, fields: [frontmatter___date] }
+//       filter: { frontmatter: { published: { eq: true } } }
+//     ) {
+//       totalCount
+//       edges {
+//         node {
+//           id
+//           excerpt(pruneLength: 250)
+//           frontmatter {
+//             title
+//             date(formatString: "YYYY MMMM Do")
+//             path
+//             tags
+//             excerpt
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+// export default IndexPage
