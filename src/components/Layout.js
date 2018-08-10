5f9abd6b0bac5af310ce3656eaf20bab7b518643
spences10 <spences10apps@gmail.com>
2018-08-10 07:02:20 +0100

    prettier

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,9 +23,8 @@ const Layout = ({ children, data }) => (
           title={data.site.siteMetadata.title}
           meta={[
             { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
+            { name: 'keywords', content: 'sample, something' }
+          ]}>
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
@@ -34,9 +33,8 @@ const Layout = ({ children, data }) => (
             margin: '0 auto',
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+            paddingTop: 0
+          }}>
           {children}
         </div>
       </>
@@ -45,7 +43,7 @@ const Layout = ({ children, data }) => (
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
