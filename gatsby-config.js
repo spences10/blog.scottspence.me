ce4867a767543712c04e098755b07ad9810b0fff
spences10 <spences10apps@gmail.com>
2018-08-12 09:06:58 +0100

    Starter config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter'
+    title: 'blog.scottspence.me'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'blog.scottspence.me',
-        short_name: 'blog.scottspence.me',
-        start_url: '/',
-        background_color: '#f7f0eb', // #755f9f
-        theme_color: '#755f9f',
-        display: 'minimal-ui',
-        icon: 'src/images/favicon.png' // This path is relative to the root of the site.
-      }
-    },
-    'gatsby-plugin-offline',
     'gatsby-plugin-netlify-cms',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/posts`,
+        path: `${__dirname}/blog`,
         name: 'posts'
       }
     },
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,95 +1,38 @@
-// const path = require('path')
+const path = require('path')
 
-// const createTagPages = (createPage, posts) => {
-//   const tagPageTemplate = path.resolve('src/templates/tags.js')
-//   const allTagsTemplate = path.resolve('src/templates/allTags.js')
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
 
-//   const postsByTags = {}
+  const blogPostTemplate = path.resolve(
+    `src/templates/blogTemplate.js`
+  )
 
-//   posts.forEach(({ node }) => {
-//     if (node.frontmatter.tags) {
-//       node.frontmatter.tags.forEach(tag => {
-//         if (!postsByTags[tag]) {
-//           postsByTags[tag] = []
-//         }
-//         postsByTags[tag].push(node)
-//       })
-//     }
-//   })
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
 
-//   const tags = Object.keys(postsByTags)
-
-//   createPage({
-//     path: `/tags`,
-//     component: allTagsTemplate,
-//     context: {
-//       tags: tags.sort()
-//     }
-//   })
-
-//   tags.forEach(tagName => {
-//     const posts = postsByTags[tagName]
-
-//     createPage({
-//       path: `/tags/${tagName}`,
-//       component: tagPageTemplate,
-//       context: {
-//         posts,
-//         tagName
-//       }
-//     })
-//   })
-// }
-
-// exports.createPages = ({ actions, graphql }) => {
-//   const { createPage } = actions
-//   const blogPostTemplate = path.resolve('src/templates/blogPost.js')
-
-//   // returns promise that will start with this graphql query
-//   return graphql(`
-//     {
-//       allMarkdownRemark(
-//         sort: { order: ASC, fields: [frontmatter___date] }
-//         filter: { frontmatter: { published: { eq: true } } }
-//         limit: 1000
-//       ) {
-//         edges {
-//           node {
-//             html
-//             id
-//             frontmatter {
-//               date
-//               path
-//               title
-//               excerpt
-//               tags
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `).then(result => {
-//     if (result.errors) {
-//       return Promise.reject(result.errors)
-//     }
-
-//     const posts = result.data.allMarkdownRemark.edges
-
-//     createTagPages(createPage, posts)
-
-//     // Create pages for each markdown file.
-//     posts.forEach(({ node }, index) => {
-//       createPage({
-//         path: node.frontmatter.path,
-//         component: blogPostTemplate,
-//         context: {
-//           prev: index === 0 ? null : posts[index - 1].node,
-//           next:
-//             index === posts.length - 1 ? null : posts[index + 1].node
-//         }
-//       })
-//     })
-
-//     return posts
-//   })
-// }
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: blogPostTemplate,
+        context: {} // additional data can be passed via context
+      })
+    })
+  })
+}
