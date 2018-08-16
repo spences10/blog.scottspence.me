d5290f468718aa8e5c1fc82ce3ea3fcc906a91dd
spences10 <spences10apps@gmail.com>
2018-08-16 07:21:26 +0100

    date format

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -25,7 +25,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY MMM Do")
         path
         title
       }
