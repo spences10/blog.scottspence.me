74bdc277920067b9aa6cbfe74817347303ed950c
spences10 <spences10apps@gmail.com>
2018-08-16 07:14:51 +0100

    Header from header!!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import Header from './Header'
 import './layout.css'
 
 const Layout = ({ children, data }) => (
