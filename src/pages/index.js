6b126ced062aaa916124a4e462de55da2448e66d
spences10 <spences10apps@gmail.com>
2018-08-16 07:29:04 +0100

    add Posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
-
-const IndexPage = () => (
+import Posts from '../components/Posts'
+const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Posts data={data} />
   </Layout>
 )
 
