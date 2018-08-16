894d59e1c99d30de5bfe1f05aa49eba46d14510c
spences10 <spences10apps@gmail.com>
2018-08-16 06:54:11 +0100

    Layout is back

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,11 @@
 import React from 'react'
-// import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
-  <div>
-    {/* <Layout> */}
+  <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    {/* </Layout> */}
-  </div>
+  </Layout>
 )
 
 export default NotFoundPage
