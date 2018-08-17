8596f119281fd417624bb0b2e3511a15fb04506b
spences10 <spences10apps@gmail.com>
2018-08-17 20:02:52 +0100

    add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: 'blog.scottspence.me'
+    title: 'blog.scottspence.me',
+    contact: {
+      github: 'https://github.com/spences10',
+      medium: 'https://medium.com/@spences10',
+      devto: 'https://dev.to/spences10',
+      twitter: 'https://twitter.com/ScottDevTweets'
+    }
   },
   plugins: [
     'gatsby-plugin-react-helmet',
