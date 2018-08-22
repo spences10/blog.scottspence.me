359cfac5af19c6323fb5bfa99fcb402d30f24b3f
spences10 <spences10apps@gmail.com>
2018-08-22 19:02:16 +0100

    try again

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -12,36 +12,34 @@ const SecondPage = () => (
 
     <form
       name="contact"
-      method="POST"
-      date-netlify="true"
+      method="post"
+      data-netlify="true"
       data-netlify-honeypot="bot-field">
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{' '}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message" />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      <input type="hidden" name="bot-field" />
+      <div className="field half first">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+      <div className="field">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" rows="6" />
+      </div>
+      <ul className="actions">
+        <li>
+          <input
+            type="submit"
+            value="Send Message"
+            className="special"
+          />
+        </li>
+        <li>
+          <input type="reset" value="Clear" />
+        </li>
+      </ul>
     </form>
 
     <Link to="/">Go back to the homepage</Link>
