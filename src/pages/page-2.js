71877121e3928a007919b8ff81be4cc16f242cf9
spences10 <spences10apps@gmail.com>
2018-08-22 18:48:07 +0100

    moar attributes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,26 +10,39 @@ const SecondPage = () => (
 
     <p>Testing Netlify forms</p>
 
-<form name="contact" method="POST" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+    <form
+      name="contact"
+      method="POST"
+      date-netlify="true"
+      data-netlify-honeypot="bot-field">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{' '}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
 
     <Link to="/">Go back to the homepage</Link>
   </Layout>
