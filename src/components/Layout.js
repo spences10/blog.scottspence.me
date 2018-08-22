af6b2568fc44230571ed7d79059762140119de88
spences10 <spences10apps@gmail.com>
2018-08-22 17:09:55 +0100

    wrapper sc in layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,8 +5,16 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import config from '../../data/siteConfig'
 import { siteMeta } from '../../data/siteMeta'
+import styled from 'styled-components'
 
 import Header from './Header'
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  maxwidth: 960;
+  padding: 0px 1.0875rem 1.45rem;
+  paddingtop: 0;
+`
 
 const Layout = ({ children, data }) => (
   <StaticQuery
@@ -25,15 +33,7 @@ const Layout = ({ children, data }) => (
           <html lang={config.siteLanguage} />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
-          }}>
-          {children}
-        </div>
+        <Wrapper>{children}</Wrapper>
       </>
     )}
   />
