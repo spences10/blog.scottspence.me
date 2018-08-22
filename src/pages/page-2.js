12000a573b2c3d3dbb5eeb67cd7327ea6a73ab4d
spences10 <spences10apps@gmail.com>
2018-08-22 17:50:52 +0100

    Netlify form test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,6 +7,28 @@ const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+
+    <p>Testing Netlify forms</p>
+
+    <form name="contact" netlify>
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <textarea name="message" />
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
