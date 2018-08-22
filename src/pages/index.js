54cb4c6ea62076dd9285765e9ce0258866983cb1
spences10 <spences10apps@gmail.com>
2018-08-22 17:01:14 +0100

    add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,38 @@
 import React from 'react'
-import { css } from 'react-emotion'
 import { Link, graphql } from 'gatsby'
-// import { rhythm } from '../utils/typography'
 import Layout from '../components/Layout'
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+
+const PostWrapper = styled.div``
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const StyledSpan = styled.span`
+  color: #bbb;
+`
 
 export default ({ data }) => {
   return (
     <Layout>
-      <div>
+      <Wrapper>
         {/* <h4>{data.allMarkdownRemark.totalCount} Posts</h4> */}
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link
-              to={node.frontmatter.path}
-              className={css`
-                text-decoration: none;
-                color: inherit;
-              `}>
-              <h3
-              // className={css`
-              //   margin-bottom: ${rhythm(1 / 4)};
-              // `}
-              >
+        {data.allMarkdownRemark.edges.map(({ node }, index) => (
+          <PostWrapper key={index}>
+            <StyledLink to={node.frontmatter.path}>
+              <h3>
                 {node.frontmatter.title}{' '}
-                <span
-                  className={css`
-                    color: #bbb;
-                  `}>
-                  — {node.frontmatter.date}
-                </span>
+                <StyledSpan>— {node.frontmatter.date}</StyledSpan>
               </h3>
               <p>{node.excerpt}</p>
-            </Link>
-          </div>
+            </StyledLink>
+          </PostWrapper>
         ))}
-      </div>
+      </Wrapper>
     </Layout>
   )
 }
