2d267b04cd9cac19cb7a7f6b9602ebc87bc09ce5
Scott Spence <spences10apps@gmail.com>
2018-08-22 18:23:06 +0100

    Test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,24 +10,26 @@ const SecondPage = () => (
 
     <p>Testing Netlify forms</p>
 
-    <form name="contact" netlify>
-      <p>
-        <label>
-          Name <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Email <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <textarea name="message" />
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
     <Link to="/">Go back to the homepage</Link>
   </Layout>
