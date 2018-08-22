fa05eb5715e178c7633506606f67758fba4cdcb4
spences10 <spences10apps@gmail.com>
2018-08-22 19:56:20 +0100

    updaty

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-// import pic11 from '../assets/images/pic11.jpg'
+import Layout from '../components/Layout'
 
 const Success = props => (
-  <div>
+  <Layout>
     <Helmet>
       <title>Success Page</title>
       <meta name="description" content="Success Page" />
@@ -16,14 +16,11 @@ const Success = props => (
           <header className="major">
             <h1>Success/Thank You Page</h1>
           </header>
-          <span className="image main">
-            {/* <img src={pic11} alt="" /> */}
-          </span>
-          <p>Thank you for contacting us!</p>
+          <p>Thank you for contacting me!</p>
         </div>
       </section>
     </div>
-  </div>
+  </Layout>
 )
 
 export default Success
