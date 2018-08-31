04bf9476341997447959a8425f9b8c4d7b9afae4
spences10 <spences10apps@gmail.com>
2018-08-31 10:57:17 +0100

    site meta

---
+++ src/components/Layout.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
+
+import Header from './Header'
+import Footer from './Footer'
+
+import {
+  BlogThemeContext,
+  BlogThemeProvider
+} from '../contexts/BlogThemeContext'
+
+import config from '../../data/siteConfig'
+import { siteMeta } from '../../data/siteMeta'
+import { reset, media } from '../theme/globalStyle'
+
+reset()
+
+const AppStyles = styled.div`
+  background-color: ${props => props.theme.background};
+  background-image: url("${props => props.background}");
+  background-attachment: fixed;
+  /*space between first post and nav bar*/
+  /* padding-top: 2.7rem; */
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    'h h h h h h h h h h h h'
+    '. . . m m m m m m . . .'
+    'f f f f f f f f f f f f';
+  ${media.giant`
+    grid-template-areas:
+      'h h h h h h h h h h h h'
+      '. . m m m m m m m m . .'
+      'f f f f f f f f f f f f';
+    /* background: goldenrod; */
+  `};
+  ${media.desktop`
+    grid-template-areas:
+      'h h h h h h h h h h h h'
+      '. m m m m m m m m m m .'
+      'f f f f f f f f f f f f';
+    /* background: dodgerblue; */
+  `};
+  ${media.tablet`
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
+      'h h h h h h h h h'
+      '. m m m m m m m .'
+      'f f f f f f f f f';
+    /* background: mediumseagreen; */
+  `};
+  ${media.phone`
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
+      'h h h h h h h h h'
+      'm m m m m m m m m'
+      'f f f f f f f f f';
+    /* background: palevioletred; */
+  `};
+`
+
+const Wrapper = styled.div`
+  grid-area: m;
+`
+
+const Layout = ({ children, data }) => (
+  <BlogThemeProvider>
+    <BlogThemeContext.Consumer>
+      {({ theme }) => (
+        <ThemeProvider theme={theme}>
+          <AppStyles>
+            <Helmet title={config.nameContent} meta={siteMeta}>
+              <html lang={config.siteLanguage} />
+            </Helmet>
+            <Header siteTitle={config.siteTitle} />
+            <Wrapper>{children}</Wrapper>
+            <Footer />
+          </AppStyles>
+        </ThemeProvider>
+      )}
+    </BlogThemeContext.Consumer>
+  </BlogThemeProvider>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Layout
