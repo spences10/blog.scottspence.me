32bf563fe17a30c5892c06233b85e9ae9c0f357d
spences10 <spences10apps@gmail.com>
2018-08-31 10:15:59 +0100

    add themes

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,6 +1,59 @@
 // https://www.styled-components.com/docs/api#injectglobal
 import { injectGlobal, css } from 'styled-components'
 
+export const themes = {
+  theme1: {
+    primary: '#f2ff49',
+    primaryAccent: '#645dd7',
+    pink: '#ff0198',
+    blue: '#01c1d6',
+    branding: '#f7e018',
+    secondary: '#ff4242',
+    background: '#b1b1cc',
+    foreground: '#fefefe',
+    border: '#044040',
+    fontDark: '#34434b',
+    fontLight: '#586368',
+    fontWhite: '#ffffff',
+    fontHeader: 'Nunito, sans, sans-serif',
+    fontBody: 'Poppins, sans-serif'
+  },
+
+  theme2: {
+    primary: '#9166ff',
+    primaryAccent: '#ecd444',
+    pink: '#ff0198',
+    blue: '#01c1d6',
+    branding: '#f7e018',
+    secondary: '#eb238e',
+    background: '#f9f9fd',
+    foreground: '#fefefe',
+    border: '#044040',
+    fontDark: '#34434b',
+    fontLight: '#586368',
+    fontWhite: '#ffffff',
+    fontHeader: 'Lato, sans-serif',
+    fontBody: 'Merriweather, sans, sans-serif'
+  },
+
+  theme3: {
+    primary: '#ff0198',
+    primaryAccent: '#ffb617',
+    pink: '#ff0198',
+    blue: '#01c1d6',
+    branding: '#01c1d6',
+    secondary: '#eb238e',
+    background: '#e6e6e6',
+    foreground: '#f7f0f0',
+    border: '#054545',
+    fontDark: '#034544',
+    fontLight: '#596869',
+    fontWhite: '#ffffff',
+    fontHeader: 'Roboto, sans, sans-serif',
+    fontBody: 'Nunito, sans-serif'
+  }
+}
+
 const sizes = {
   giant: 1170,
   desktop: 992,
