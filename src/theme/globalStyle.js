66413cd526d640645f9da75d668763abb6085fd7
spences10 <spences10apps@gmail.com>
2018-08-31 06:34:51 +0100

    add media query

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,5 +1,28 @@
 // https://www.styled-components.com/docs/api#injectglobal
-import { injectGlobal } from 'styled-components'
+import { injectGlobal, css } from 'styled-components'
+
+const sizes = {
+  giant: 1170,
+  desktop: 992,
+  tablet: 768,
+  phone: 376
+}
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce(
+  (accumulator, label) => {
+    // use em in breakpoints to work properly cross-browser and support users
+    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+    const emSize = sizes[label] / 16
+    accumulator[label] = (...args) => css`
+      @media (max-width: ${emSize}em) {
+        ${css(...args)};
+      }
+    `
+    return accumulator
+  },
+  {}
+)
 
 export const reset = () =>
   injectGlobal`
