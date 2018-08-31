65addea5d34b89e675df6aeb4d4784de262adc66
spences10 <spences10apps@gmail.com>
2018-08-31 06:35:13 +0100

    add app styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,15 +9,57 @@ import { siteMeta } from '../../data/siteMeta'
 
 import Header from './Header'
 
-import { reset } from '../theme/globalStyle'
+import { reset, media } from '../theme/globalStyle'
 
 reset()
 
+const AppStyles = styled.div`
+  background-color: ${props => props.theme.background};
+  background-image: url("${props => props.background}");
+  background-attachment: fixed;
+  /*space between first post and nav bar*/
+  /* padding-top: 2.7rem; */
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    'h h h h h h h h h h h h'
+    '. . . m m m m m m . . .'
+    'f f f f f f f f f f f f';
+  ${media.giant`
+    grid-template-areas:
+      'h h h h h h h h h h h h'
+      '. . m m m m m m m m . .'
+      'f f f f f f f f f f f f';
+    /* background: goldenrod; */
+  `};
+  ${media.desktop`
+    grid-template-areas:
+      'h h h h h h h h h h h h'
+      '. m m m m m m m m m m .'
+      'f f f f f f f f f f f f';
+    /* background: dodgerblue; */
+  `};
+  ${media.tablet`
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
+      'h h h h h h h h h'
+      '. m m m m m m m .'
+      'f f f f f f f f f';
+    /* background: mediumseagreen; */
+  `};
+  ${media.phone`
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-areas:
+      'h h h h h h h h h'
+      'm m m m m m m m m'
+      'f f f f f f f f f';
+    /* background: palevioletred; */
+  `};
+`
+
 const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 960;
-  padding: 0px 1.0875rem 1.45rem;
-  padding-top: 0;
+  grid-area: m;
 `
 
 const Layout = ({ children, data }) => (
@@ -37,7 +79,9 @@ const Layout = ({ children, data }) => (
           <html lang={config.siteLanguage} />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <Wrapper>{children}</Wrapper>
+        <AppStyles>
+          <Wrapper>{children}</Wrapper>
+        </AppStyles>
       </>
     )}
   />
