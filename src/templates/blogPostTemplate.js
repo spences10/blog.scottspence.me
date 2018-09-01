64a60032ea65000db572b122f2c99a0b1af12b4d
spences10 <spences10apps@gmail.com>
2018-09-01 11:36:57 +0100

    add styled nav buttons and shared components

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
+
 // import { Dump } from '../utils/helpers'
-// import styled from 'styled-components'
+import { HappyButton } from '../components/Shared'
 
 import config from '../../data/siteConfig'
 import { siteMeta } from '../../data/siteMeta'
@@ -13,8 +15,56 @@ import { siteMeta } from '../../data/siteMeta'
 require('prismjs/themes/prism-solarizedlight.css')
 
 // Title
+// Date
 // Content
 // Navigation
+
+const Title = styled.h1`
+  margin: 0rem;
+  padding: 0rem;
+  font-family: ${({ theme }) => theme.fontHeader};
+  color: ${({ theme }) => theme.secondary};
+  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+`
+
+const TitleDate = styled.h4`
+  margin: 0rem;
+  padding: 0rem;
+  font-family: ${({ theme }) => theme.fontBody};
+  color: ${({ theme }) => theme.fontLight};
+  border-bottom: 1px solid ${({ theme }) => theme.background};
+`
+
+const PostWrapper = styled.div`
+  margin: 0.5rem;
+  padding: 1.5rem;
+  background: ${({ theme }) => theme.foreground};
+  border: 1px solid ${props => props.border};
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  font-family: ${props => props.theme.fontBody};
+  color: ${props => props.theme.fontDark};
+`
+
+const LinksWrapper = styled.div`
+  display: grid;
+  grid-template-areas: 'prev next';
+`
+
+const LinkWrapper = styled.div`
+  display: grid;
+  justify-items: ${props => props.justify};
+`
+
+const PrevNextButton = HappyButton.extend`
+  margin: 0.5rem 0rem;
+  padding: 0.5rem;
+  grid-area: ${props => props.area};
+`
 
 const blogPostLayout = ({ data, pageContext }) => {
   const post = data.markdownRemark
@@ -27,31 +77,40 @@ const blogPostLayout = ({ data, pageContext }) => {
         <html lang={config.siteLanguage} />
       </Helmet>
       {/* <Dump props={data} /> */}
-      <div>
-        <h1>{post.frontmatter.title}</h1>
+      <PostWrapper>
+        <Title>{post.frontmatter.title}</Title>
+        <TitleDate>{post.frontmatter.date}</TitleDate>
         {/* <Markdown source={post.html} /> */}
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
-      <div>
-        {prev === false ? null : (
-          <div>
-            {prev && (
-              <Link to={prev.frontmatter.path}>
-                <button>{prev.frontmatter.title}</button>
-              </Link>
+        <LinksWrapper>
+          <LinkWrapper justify={'start'}>
+            {prev === false ? null : (
+              <div>
+                {prev && (
+                  <Link to={prev.frontmatter.path}>
+                    <PrevNextButton area={'prev'}>
+                      {prev.frontmatter.title}
+                    </PrevNextButton>
+                  </Link>
+                )}
+              </div>
             )}
-          </div>
-        )}
-        {next === false ? null : (
-          <div>
-            {next && (
-              <Link to={next.frontmatter.path}>
-                <button>{next.frontmatter.title}</button>
-              </Link>
+          </LinkWrapper>
+          <LinkWrapper justify={'end'}>
+            {next === false ? null : (
+              <div>
+                {next && (
+                  <Link to={next.frontmatter.path}>
+                    <PrevNextButton area={'next'}>
+                      {next.frontmatter.title}
+                    </PrevNextButton>
+                  </Link>
+                )}
+              </div>
             )}
-          </div>
-        )}
-      </div>
+          </LinkWrapper>
+        </LinksWrapper>
+      </PostWrapper>
     </Layout>
   )
 }
