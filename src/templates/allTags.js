2eff3cf5dbbf86e066092f667d772e0d588354cb
spences10 <spences10apps@gmail.com>
2018-09-15 14:59:08 +0100

    Add Layout

--- src/templates/allTags.js
+++ src/templates/allTags.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Layout from '../components/Layout'
+
 const AllTagsTemplate = ({ data, pageContext }) => {
   const { tags } = pageContext
   return (
-    <div style={{ fontFamily: 'avenir' }}>
-      <div>
-        <ul>
-          {tags.map((tagName, index) => {
-            return (
-              <li key={index}>
-                <Link to={`/tags/${tagName}`}>{tagName}</Link>
-              </li>
-            )
-          })}
-        </ul>
+    <Layout>
+      <div style={{ fontFamily: 'avenir' }}>
+        <div>
+          <ul>
+            {tags.map((tagName, index) => {
+              return (
+                <li key={index}>
+                  <Link to={`/tags/${tagName}`}>{tagName}</Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
--- src/templates/singleTag.js
+++ src/templates/singleTag.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Layout from '../components/Layout'
+
 const SingleTagTemplate = ({ data, pageContext }) => {
   const { posts, tagName } = pageContext
   return (
-    <div style={{ fontFamily: 'avenir' }}>
-      <div>Posts about {`${tagName}`}</div>
-      <div>
-        <ul>
-          {posts.map((post, index) => {
-            return (
-              <li key={index}>
-                <Link to={post.frontmatter.path}>
-                  {post.frontmatter.title}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
+    <Layout>
+      <div style={{ fontFamily: 'avenir', height: '70vh' }}>
+        <div>Posts about {`${tagName}`}</div>
+        <div>
+          <ul>
+            {posts.map((post, index) => {
+              return (
+                <li key={index}>
+                  <Link to={post.frontmatter.path}>
+                    {post.frontmatter.title}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
