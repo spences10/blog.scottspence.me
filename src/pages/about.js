4a2465e707b2691d43a32d2e6f8d5288dfaff4cd
spences10 <spences10apps@gmail.com>
2018-09-18 06:29:19 +0100

    add layout for about work on this shit!

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const About = props => {
   return (
-    <React.Fragment>
+    <Layout>
       <p>welcome to about</p>
-    </React.Fragment>
+    </Layout>
   )
 }
 
