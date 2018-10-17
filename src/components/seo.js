5347da53306e5c170016314c1fcc275b71678ab9
spences10 <spences10apps@gmail.com>
2018-10-17 06:55:04 +0100

    add title template

--- src/components/seo.js
+++ src/components/seo.js
@@ -27,7 +27,7 @@ const SEO = ({ title, description, image, pathname, article }) => (
 
       return (
         <>
-          <Helmet>
+          <Helmet title={seo.title} titleTemplate={titleTemplate}>
             <meta name="description" content={seo.description} />
             <meta name="image" content={seo.image} />
             {seo.url && <meta property="og:url" content={seo.url} />}
