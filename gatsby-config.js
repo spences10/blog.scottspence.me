71e048f583517ed1b947d6abe28f0988200c96dc
spences10 <spences10apps@gmail.com>
2018-11-05 05:56:43 +0000

    feed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,7 +117,64 @@ module.exports = {
       }
     },
     'gatsby-plugin-styled-components',
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url:
+                    site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid:
+                    site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [
+                    { 'content:encoded': edge.node.html }
+                  ]
+                })
+              })
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: {frontmatter: { draft: { ne: true } }}
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: '/rss.xml',
+            title: 'Gatsby RSS Feed'
+          }
+        ]
+      }
+    },
     // this has to stay at the end of the array
     'gatsby-plugin-netlify'
   ]
