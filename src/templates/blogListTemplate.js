54e1e56f0a671d954dd61caac46bf47d156f6a3f
spences10 <spences10apps@gmail.com>
2018-11-11 09:16:22 +0000

    add blog list template

---
+++ src/templates/blogListTemplate.js
@@ -0,0 +1,42 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import blogPostLayout from './blogPostTemplate'
+
+const BlogListLayout = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges
+  return (
+    <Layout>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.frontmatter.path
+        return <div key={node.frontmatter.path}>{title}</div>
+      })}
+    </Layout>
+  )
+}
+
+// BlogListLayout.propTypes = {
+//   props: PropTypes.props
+// }
+
+export default blogPostLayout
+
+export const blogListQuery = graphql`
+  query blogListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
