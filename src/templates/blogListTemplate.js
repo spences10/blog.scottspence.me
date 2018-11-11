46ee66ec64d61742069b896abf0c42f8e5419e76
spences10 <spences10apps@gmail.com>
2018-11-11 16:53:38 +0000

    reuse shared components

--- src/templates/blogListTemplate.js
+++ src/templates/blogListTemplate.js
@@ -6,57 +6,16 @@ import styled from 'styled-components'
 import Layout from '../components/Layout'
 // import { Dump } from '../utils/helpers'
 
-import { HappyButton } from '../components/Shared'
-
-const Wrapper = styled.div``
-
-const PostWrapper = styled.div`
-  margin: 0.5rem;
-  padding: 1rem 1.5rem;
-  border: 1px solid ${({ theme }) => theme.border};
-  border-radius: 4px;
-  background: white;
-  line-height: 1.5;
-  font-family: ${({ theme }) => theme.fontBody};
-  color: ${({ theme }) => theme.fontLight};
-  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
-`
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
-`
-
-const PostTitle = styled.div`
-  font-family: ${({ theme }) => theme.fontHeader};
-  font-weight: 700;
-  font-size: 2rem;
-  color: ${({ theme }) => theme.fontLight};
-  margin: 0rem;
-  padding: 0rem;
-  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-`
-
-const PostedDate = styled.p`
-  margin: 0.05rem;
-  color: ${({ theme }) => theme.fontLight};
-`
-
-const LinksWrapper = styled.div`
-  display: grid;
-  grid-template-areas: 'prev next';
-`
-
-const LinkWrapper = styled.div`
-  display: grid;
-  justify-items: ${props => props.justify};
-`
-
-const PrevNextButton = styled(HappyButton)`
-  margin: 0.5rem 0rem;
-  padding: 0.5rem;
-  grid-area: ${props => props.area};
-`
+import {
+  Wrapper,
+  PostWrapper,
+  StyledLink,
+  PostTitle,
+  PostedDate,
+  LinksWrapper,
+  LinkWrapper,
+  PrevNextButton
+} from '../components/Shared'
 
 const BlogListLayout = ({ data, pageContext }) => {
   const posts = data.allMarkdownRemark.edges
