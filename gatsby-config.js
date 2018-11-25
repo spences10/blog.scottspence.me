3820c2d827cd90a51ebeaf4a95ef1d610814855a
spences10 <spences10apps@gmail.com>
2018-11-25 22:11:34 +0000

    add and config gatsby-remark-embed-video

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,6 +116,23 @@ module.exports = {
         anonymize: false
       }
     },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-embed-video',
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+            }
+          }
+        ]
+      }
+    },
     'gatsby-plugin-styled-components',
     // this has to stay at the end of the array
     'gatsby-plugin-netlify'
