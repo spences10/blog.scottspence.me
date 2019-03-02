e381083f9a36d333632469cf9eea5b13a7e051b3
spences10 <spences10apps@gmail.com>
2019-03-02 17:35:21 +0000

    Cased

---
+++ src/components/SEO.js
@@ -0,0 +1,91 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import Helmet from 'react-helmet'
+import useSiteMetadata from './SiteMetadata'
+
+const SEO = ({ title, description }) => {
+  const {
+    title,
+    description,
+    image,
+    imageLink,
+    siteLanguage,
+    titleTemplate,
+    twitterUsername
+  } = useSiteMetadata()
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        siteLanguage
+      }}
+      title={title}
+      titleTemplate={titleTemplate}
+      meta={[
+        {
+          name: `description`,
+          content: description
+        },
+        {
+          property: `og:title`,
+          content: title
+        },
+        {
+          property: `og:description`,
+          content: description
+        },
+        {
+          property: `og:type`,
+          content: `website`
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`
+        },
+        {
+          name: `twitter:image`,
+          content: imageLink
+        },
+        {
+          name: `twitter:creator`,
+          content: twitterUsername
+        },
+        {
+          name: `twitter:title`,
+          content: title
+        },
+        {
+          name: `twitter:description`,
+          content: description
+        }
+      ]
+        .concat(
+          keywords.length > 0
+            ? {
+                name: `keywords`,
+                content: keywords.join(`, `)
+              }
+            : []
+        )
+        .concat(meta)}
+    />
+  )
+}
+
+export default SEO
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  pathname: PropTypes.string,
+  article: PropTypes.bool
+}
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  pathname: null,
+  article: false
+}
