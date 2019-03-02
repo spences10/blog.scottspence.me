2690dbd5c6f399e89adcad9e9605af74eb384d8e
spences10 <spences10apps@gmail.com>
2019-03-02 18:58:24 +0000

    default prop typed!

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Helmet from 'react-helmet'
+import { Dump } from '../utils/helpers'
 import useSiteMetadata from './SiteMetadata'
 
-const SEO = ({ keywords, meta, description, title, image }) => {
+const SEO = ({
+  title,
+  description,
+  image,
+  keywords,
+  meta,
+  pathname
+}) => {
   const {
     description: defaultDescription,
     imageLink: defaultImage,
     siteLanguage,
+    siteUrl,
     title: defaultTitle,
     titleTemplate,
     twitterUsername
@@ -16,81 +25,83 @@ const SEO = ({ keywords, meta, description, title, image }) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: `${image || defaultImage}`
+    image: `${image || defaultImage}`,
+    url: `${siteUrl}${pathname || '/'}`
   }
 
   return (
-    <Helmet
-      htmlAttributes={{
-        siteLanguage
-      }}
-      title={seo.title}
-      titleTemplate={titleTemplate}
-      meta={[
-        {
-          name: `description`,
-          content: seo.description
-        },
-        {
-          property: `og:title`,
-          content: seo.title
-        },
-        {
-          property: `og:description`,
-          content: seo.description
-        },
-        {
-          property: `og:type`,
-          content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:image`,
-          content: seo.imageLink
-        },
-        {
-          name: `twitter:creator`,
-          content: twitterUsername
-        },
-        {
-          name: `twitter:title`,
-          content: seo.title
-        },
-        {
-          name: `twitter:description`,
-          content: seo.description
-        }
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `)
-              }
-            : []
-        )
-        .concat(meta)}
-    />
+    <>
+      <Dump props={seo} />
+      <Helmet
+        htmlAttributes={{
+          siteLanguage
+        }}
+        title={seo.title}
+        titleTemplate={titleTemplate}
+        meta={[
+          {
+            name: `description`,
+            content: seo.description
+          },
+          {
+            property: `og:title`,
+            content: seo.title
+          },
+          {
+            property: `og:description`,
+            content: seo.description
+          },
+          {
+            property: `og:type`,
+            content: `website`
+          },
+          {
+            name: `twitter:card`,
+            content: `summary_large_image`
+          },
+          {
+            name: `twitter:image`,
+            content: seo.image
+          },
+          {
+            name: `twitter:creator`,
+            content: twitterUsername
+          },
+          {
+            name: `twitter:title`,
+            content: seo.title
+          },
+          {
+            name: `twitter:description`,
+            content: seo.description
+          }
+        ]
+          .concat(
+            keywords.length > 0
+              ? {
+                  name: `keywords`,
+                  content: keywords.join(`, `)
+                }
+              : []
+          )
+          .concat(meta)}
+      />
+    </>
   )
 }
 
 export default SEO
 
-SEO.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string,
-  pathname: PropTypes.string,
-  article: PropTypes.bool
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  keywords: []
 }
 
-SEO.defaultProps = {
-  title: null,
-  description: null,
-  image: null,
-  pathname: null,
-  article: false
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.array,
+  keywords: PropTypes.arrayOf(PropTypes.string),
+  title: PropTypes.string.isRequired
 }
