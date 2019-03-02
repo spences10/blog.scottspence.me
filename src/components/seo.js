b7ea0355c06f6ed258287ee4408cb54342deb18c
spences10 <spences10apps@gmail.com>
2019-03-02 15:28:35 +0000

    remove static query component

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,75 +1,49 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Helmet from 'react-helmet'
+import useSiteMetadata from './SiteMetadata'
 
-const SEO = ({ title, description, image, pathname, article }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        siteMetadata: {
-          defaultTitle,
-          titleTemplate,
-          defaultDescription,
-          siteUrl,
-          defaultImage,
-          twitterUsername
-        }
-      }
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: `${image || defaultImage}`,
-        url: `${siteUrl}${pathname || '/'}`
-      }
+const SEO = ({ title, description, image, pathname, article }) => {
+  const { title, description, image, siteUrl } = useSiteMetadata()
 
-      return (
-        <>
-          <Helmet title={seo.title} titleTemplate={titleTemplate}>
-            <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} />
-            {seo.url && <meta property="og:url" content={seo.url} />}
-            {(article ? true : null) && (
-              <meta property="og:type" content="article" />
-            )}
-            {seo.title && (
-              <meta property="og:title" content={seo.title} />
-            )}
-            {seo.description && (
-              <meta
-                property="og:description"
-                content={seo.description}
-              />
-            )}
-            {seo.image && (
-              <meta property="og:image" content={seo.image} />
-            )}
-            <meta name="twitter:card" content="summary_large_image" />
-            {twitterUsername && (
-              <meta
-                name="twitter:creator"
-                content={twitterUsername}
-              />
-            )}
-            {seo.title && (
-              <meta name="twitter:title" content={seo.title} />
-            )}
-            {seo.description && (
-              <meta
-                name="twitter:description"
-                content={seo.description}
-              />
-            )}
-            {seo.image && (
-              <meta name="twitter:image" content={seo.image} />
-            )}
-          </Helmet>
-        </>
-      )
-    }}
-  />
-)
+  return (
+    <>
+      <Helmet title={seo.title} titleTemplate={titleTemplate}>
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
+        {seo.url && <meta property="og:url" content={seo.url} />}
+        {(article ? true : null) && (
+          <meta property="og:type" content="article" />
+        )}
+        {seo.title && (
+          <meta property="og:title" content={seo.title} />
+        )}
+        {seo.description && (
+          <meta property="og:description" content={seo.description} />
+        )}
+        {seo.image && (
+          <meta property="og:image" content={seo.image} />
+        )}
+        <meta name="twitter:card" content="summary_large_image" />
+        {twitterUsername && (
+          <meta name="twitter:creator" content={twitterUsername} />
+        )}
+        {seo.title && (
+          <meta name="twitter:title" content={seo.title} />
+        )}
+        {seo.description && (
+          <meta
+            name="twitter:description"
+            content={seo.description}
+          />
+        )}
+        {seo.image && (
+          <meta name="twitter:image" content={seo.image} />
+        )}
+      </Helmet>
+    </>
+  )
+}
 
 export default SEO
 
