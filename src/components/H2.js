7f6f34b6112aad27f4edd53a3ea3ae8e52cb900b
spences10 <spences10apps@gmail.com>
2019-06-06 07:13:13 +0100

    update h2 link on hover

--- src/components/H2.js
+++ src/components/H2.js
@@ -3,7 +3,19 @@ import styled from 'styled-components';
 export const StyledH2 = styled.h2`
   font-family: ${props => props.theme.fontBody};
   color: ${props => props.theme.fontDark};
-  /* svg {
-    position: absolute;
-  } */
+  a {
+    float: left;
+    padding-right: 4px;
+    margin-left: -20px;
+  }
+  svg {
+    visibility: hidden;
+  }
+  &:hover {
+    a {
+      svg {
+        visibility: visible;
+      }
+    }
+  }
 `;
