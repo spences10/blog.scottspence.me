b1a117a40dae1d53c8a411ce2cf29154a9bc9995
spences10 <spences10apps@gmail.com>
2019-06-07 20:25:12 +0100

    add grouped elements

---
+++ src/components/elements/GroupedElements.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const inlineCode = `
+  background-color: #fafafa;
+  font-family: dm;
+`;
+
+export const InlineCode = styled.code`
+  ${inlineCode}
+`;
+
+export const StyledP = styled.p`
+  code {
+    ${inlineCode};
+  }
+`;
