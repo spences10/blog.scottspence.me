2ce436a7053afe7ee9d1fa202019a39712d5d549
spences10 <spences10apps@gmail.com>
2019-06-11 06:47:22 +0100

    add export with props

--- src/components/elements/GroupedElements.js
+++ src/components/elements/GroupedElements.js
@@ -1,15 +1,22 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export const InlineCode = styled.code`
+export const StyledInlineCode = styled.code`
   font-family: dm;
-  background-color: ${({ theme }) => theme.background};
+  background-color: #fafafa;
+  /* background-color: ${({ theme }) => theme.background}; */
 `;
+
+export const InlineCode = ({ children }) => (
+  <StyledInlineCode>{children}</StyledInlineCode>
+);
 
 export const StyledP = styled.p`
   iframe {
   }
   code {
     font-family: dm;
-    background-color: ${({ theme }) => theme.background};
+    background-color: #fafafa;
+    /* background-color: ${({ theme }) => theme.background}; */
   }
 `;
