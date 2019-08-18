831cb6ae1899dc63c3a87a2bab90e0d87a548de1
spences10 <spences10apps@gmail.com>
2019-08-18 08:30:20 +0100

    stop background transitions

--- src/contexts/BlogThemeContext.js
+++ src/contexts/BlogThemeContext.js
@@ -1,36 +1,36 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import { themes } from '../theme/globalStyle'
-import { HERO } from '../theme/heroPatterns'
+import PropTypes from 'prop-types';
+import React from 'react';
+import { themes } from '../theme/globalStyle';
+import { HERO } from '../theme/heroPatterns';
 
 // Context is made up of two things
 // Provider - Single as close to top level as possible
 // Consumer - Multiple have multiple consumers
-export const BlogThemeContext = React.createContext()
+export const BlogThemeContext = React.createContext();
 
 export class BlogThemeProvider extends React.Component {
   state = {
     theme: themes['theme1'],
-    background: HERO[0]
-  }
+    background: HERO[0],
+  };
 
   handleThemeChange = e => {
-    const key = e.target.value
-    const theme = themes[key]
-    this.setState({ theme })
-  }
+    const key = e.target.value;
+    const theme = themes[key];
+    this.setState({ theme });
+  };
 
   componentDidMount() {
-    this.interval = setInterval(() => {
-      const keys = Object.keys(HERO)
-      const background =
-        HERO[keys[(keys.length * Math.random()) << 0]]
-      this.setState({ background })
-    }, 30 * 1000)
+    // this.interval = setInterval(() => {
+    //   const keys = Object.keys(HERO)
+    //   const background =
+    //     HERO[keys[(keys.length * Math.random()) << 0]]
+    //   this.setState({ background })
+    // }, 30 * 1000)
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval)
+    clearInterval(this.interval);
   }
 
   render() {
@@ -38,14 +38,14 @@ export class BlogThemeProvider extends React.Component {
       <BlogThemeContext.Provider
         value={{
           ...this.state,
-          handleThemeChange: this.handleThemeChange
+          handleThemeChange: this.handleThemeChange,
         }}>
         {this.props.children}
       </BlogThemeContext.Provider>
-    )
+    );
   }
 }
 
 BlogThemeProvider.propTypes = {
-  children: PropTypes.any
-}
+  children: PropTypes.any,
+};
