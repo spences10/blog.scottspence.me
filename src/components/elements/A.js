3546d2ca5dce8d0967b13670a8ce3e87c56ac05b
spences10 <spences10apps@gmail.com>
2019-08-21 06:27:30 +0100

    underline animation

---
+++ src/components/elements/A.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const StyledATag = styled.a`
+  border-bottom: 2px solid ${({ theme }) => theme.primary};
+  background-image: linear-gradient(
+    120deg,
+    ${({ theme }) => theme.primary} 0%,
+    ${({ theme }) => theme.primary} 100%
+  );
+  background-repeat: no-repeat;
+  background-size: 100% 0em;
+  background-position: 0 100%;
+  transition: background-size 0.125s ease-in;
+  &:hover {
+    border-color: ${({ theme }) => theme.primary};
+    color: black;
+    background-size: 100% 100%;
+  }
+`;
+
+export const AnchorTag = ({ children }) => (
+  <StyledATag>{children}</StyledATag>
+);
