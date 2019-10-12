7bdbdeb56f2c974e139ccf632a582af35b1d12fb
spences10 <spences10apps@gmail.com>
2019-10-12 13:22:23 +0100

    prep for jsonld

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -36,38 +36,14 @@ export const SEO = ({
   return (
     <>
       <Helmet title={seo.title} titleTemplate={titleTemplate}>
+        <html lang={siteLanguage} />
         <meta name="description" content={seo.description} />
         <meta name="image" content={seo.image} />
-        <html lang={siteLanguage} />
-        {seo.url && <meta property="og:url" content={seo.url} />}
-        {(article ? true : null) && (
-          <meta property="og:type" content="article" />
-        )}
-        {seo.title && (
-          <meta property="og:title" content={seo.title} />
-        )}
-        {seo.description && (
-          <meta property="og:description" content={seo.description} />
-        )}
-        {seo.image && (
-          <meta property="og:image" content={seo.image} />
-        )}
-        <meta name="twitter:card" content="summary_large_image" />
-        {twitterUsername && (
-          <meta name="twitter:creator" content={twitterUsername} />
-        )}
-        {seo.title && (
-          <meta name="twitter:title" content={seo.title} />
-        )}
-        {seo.description && (
-          <meta
-            name="twitter:description"
-            content={seo.description}
-          />
-        )}
-        {seo.image && (
-          <meta name="twitter:image" content={seo.image} />
-        )}
+        <meta name="blog-scott-spence" content="Scott Spence Blog" />
+        {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
+        {/* {!article && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>} */}
+        {/* {article && <script type="application/ld+json">{JSON.stringify(schemaArticle)}</script>} */}
+        {/* <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script> */}
       </Helmet>
       <Facebook
         desc={seo.description}
