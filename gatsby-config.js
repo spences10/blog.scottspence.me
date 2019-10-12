4607eeef4a62a5b1c9d68fe3a804df8db7ae0176
spences10 <spences10apps@gmail.com>
2019-10-12 14:13:42 +0100

    date to iso string

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ const siteMetadata = {
   themeColour: `#755f9f`,
   siteLanguage: `en-GB`,
   siteLocale: `en_gb`,
-  lastBuildDate: new Date(Date.now()).toLocaleDateString(),
+  lastBuildDate: new Date(Date.now()).toISOString(),
 };
 
 module.exports = {
