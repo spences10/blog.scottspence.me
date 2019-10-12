dbf0228602ff4318eb8281055e1b3d5fa88bba4b
spences10 <spences10apps@gmail.com>
2019-10-12 13:43:31 +0100

    add published date

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -11,6 +11,7 @@ export const SEO = ({
   description,
   image,
   pathname,
+  publishedDate,
 }) => {
   const {
     description: defaultDescription,
@@ -21,6 +22,8 @@ export const SEO = ({
     title: defaultTitle,
     titleTemplate,
     twitterUsername,
+    developerName,
+    lastBuildDate,
   } = useSiteMetadata();
 
   const imagePath = `${siteUrl}${image.publicURL}`;
@@ -33,6 +36,127 @@ export const SEO = ({
     url: `${siteUrl}${pathname || '/'}`,
   };
 
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const builtDate = new Date(lastBuildDate).toLocaleDateString(
+    siteLanguage,
+    options
+  );
+
+  // schema.org in JSONLD format
+  // https://developers.google.com/search/docs/guides/intro-structured-data
+  // You can fill out the 'author', 'creator' with more data or another type (e.g. 'Organization')
+
+  const schemaOrgWebPage = {
+    '@context': 'http://schema.org',
+    '@type': 'WebPage',
+    url: siteUrl,
+    headline: description,
+    inLanguage: siteLanguage,
+    mainEntityOfPage: siteUrl,
+    description: defaultDescription,
+    name: defaultTitle,
+    author: {
+      '@type': 'Person',
+      name: developerName,
+    },
+    copyrightHolder: {
+      '@type': 'Person',
+      name: developerName,
+    },
+    copyrightYear: '2019',
+    creator: {
+      '@type': 'Person',
+      name: developerName,
+    },
+    publisher: {
+      '@type': 'Person',
+      name: developerName,
+    },
+    datePublished: '2019-10-12T13:28:00+01:00',
+    dateModified: builtDate,
+    image: {
+      '@type': 'ImageObject',
+      url: `${siteUrl}${defaultImage}`,
+    },
+  };
+
+  // Initial breadcrumb list
+
+  const itemListElement = [
+    {
+      '@type': 'ListItem',
+      item: {
+        '@id': siteUrl,
+        name: 'Homepage',
+      },
+      position: 1,
+    },
+  ];
+
+  let schemaArticle = null;
+
+  if (article) {
+    schemaArticle = {
+      '@context': 'http://schema.org',
+      '@type': 'Article',
+      author: {
+        '@type': 'Person',
+        name: developerName,
+      },
+      copyrightHolder: {
+        '@type': 'Person',
+        name: developerName,
+      },
+      copyrightYear: '2019',
+      creator: {
+        '@type': 'Person',
+        name: developerName,
+      },
+      publisher: {
+        '@type': 'Organization',
+        name: developerName,
+        logo: {
+          '@type': 'ImageObject',
+          url: `${siteUrl}${defaultImage}`,
+        },
+      },
+      datePublished: publishedDate,
+      dateModified: publishedDate,
+      description: seo.description,
+      headline: seo.title,
+      inLanguage: siteLanguage,
+      url: seo.url,
+      name: seo.title,
+      image: {
+        '@type': 'ImageObject',
+        url: seo.image,
+      },
+      mainEntityOfPage: seo.url,
+    };
+    // Push current blogpost into breadcrumb list
+    itemListElement.push({
+      '@type': 'ListItem',
+      item: {
+        '@id': seo.url,
+        name: seo.title,
+      },
+      position: 2,
+    });
+  }
+
+  const breadcrumb = {
+    '@context': 'http://schema.org',
+    '@type': 'BreadcrumbList',
+    description: 'Breadcrumbs list',
+    name: 'Breadcrumbs',
+    itemListElement,
+  };
+
   return (
     <>
       <Helmet title={seo.title} titleTemplate={titleTemplate}>
@@ -41,9 +165,19 @@ export const SEO = ({
         <meta name="image" content={seo.image} />
         <meta name="blog-scott-spence" content="Scott Spence Blog" />
         {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
-        {/* {!article && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>} */}
-        {/* {article && <script type="application/ld+json">{JSON.stringify(schemaArticle)}</script>} */}
-        {/* <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script> */}
+        {!article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaOrgWebPage)}
+          </script>
+        )}
+        {article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaArticle)}
+          </script>
+        )}
+        <script type="application/ld+json">
+          {JSON.stringify(breadcrumb)}
+        </script>
       </Helmet>
       <Facebook
         desc={seo.description}
