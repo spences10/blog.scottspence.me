a3d7aab1e167b8fae3d403ee53d330c93bea6c10
spences10 <spences10apps@gmail.com>
2019-10-13 07:27:01 +0100

    position and add links

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 100px;
+  left: 50px;
+`;
 
 export const TOC = ({ toc }) => {
   return (
-    <>
+    <Wrapper>
       {toc.map((line, i) => {
-        return <p>{line.title}</p>;
+        return (
+          <>
+            <p>
+              <a href={line.url}>{line.title}</a>
+            </p>
+          </>
+        );
       })}
-    </>
+    </Wrapper>
   );
 };
